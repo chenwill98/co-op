befc22ed78470c350cf02ec67a3a60f9f6726ffe
Will Chen <chenwill98@gmail.com>
2025-03-04 02:39:29 -0500

    removed unnecessary stuff

--- src/frontend/tailwind.config.ts
+++ src/frontend/tailwind.config.ts
@@ -6,16 +6,6 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        'custom-great': '#508D69',
-        'custom-good': '#9ADE7B',
-        'custom-fair': '#EEF296',
-        'custom-poor': '#FF8F8F',
-      },
-    },
-  },
   plugins: [
     require('daisyui'),
   ],
